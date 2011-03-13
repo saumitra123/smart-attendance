c6c6ca51edf65ae8ca75fb61650f607e2e0a1be5
Saumitra Chowdhury <saumitra@smartitengineering.com>
2011-03-13 15:13:47 +0600

    Created 4 module
    Signed-off-by: Saumitra Chowdhury <saumitra@smartitengineering.com>

---
+++ smart-attendance-api-impl/src/main/java/com/smartitengineering/smartattendance/smartattendanceapiimpl/App.java
@@ -0,0 +1,13 @@
+package com.smartitengineering.smartattendance.smartattendanceapiimpl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
